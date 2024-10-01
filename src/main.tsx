8853f3d46252ecce203e99b45041d4d5818e949f
Peter Owira <petermakongul@gmail.com>
2024-10-01 17:28:11 +0300

    Update Todo Web App

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,11 @@ import App from "./App.tsx";
 import "./index.css";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import store from "./app/store.ts";
+import store from "./store.ts";
+import { fetchTodos } from "./services/todosSlice.ts";
+
+// Prefetch your data starts here
+store.dispatch(fetchTodos());
 
 createRoot(document.getElementById("root") as HTMLElement).render(
 	<StrictMode>
